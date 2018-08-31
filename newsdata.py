0417b611a75a1189ff2d23114ae331ac93f61310
Zhen Bian <zbian002@ucr.edu>
2018-08-31 16:40:04 -0700

    Update the most popular three articles query in newsdata.py

--- newsdata.py
+++ newsdata.py
@@ -2,4 +2,9 @@ import psycopg2, bleach
 
 # Database queries
 # Database query 1: The most popular three articles of all time:
-most_popular_articles = "select "
+most_popular_articles = """select articles.title, count(*) as num
+                    from articles, log
+                    where log.status = '200 OK'
+                    and articles.slug = substring(log.path, 10)
+                    order by num desc
+                    limit 3;"""
