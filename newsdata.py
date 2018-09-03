1d87a96ee7195e2828c99a0633574ac0a49ec622
Zhen Bian <zbian002@ucr.edu>
2018-09-03 01:56:24 -0700

    All the functions works good and add results.txt for the print results of all three queries

--- newsdata.py
+++ newsdata.py
@@ -22,7 +22,19 @@ most_popular_authors = """select authors.name, count(*) as num
                           order by num desc;"""
 
 # Database query 3: On which days did more than 1% of requests lead to errors?
-#error_request = """select time """
+error_requests = """select total.date,
+                    round(errors.error_requests * 1.0 / total.requests * 100, 2) as percent
+                    from (
+                        select date(time), count(*) as error_requests
+                        from log where status like '404%'
+                        group by date(time)
+                    ) as errors, (
+		                  select date(time), count(*) as requests
+                          from log group by date(log.time)
+                    ) as total
+                    where total.date = errors.date
+                    and round(errors.error_requests * 1.0 / total.requests * 100, 2) > 1
+                    order by percent desc;"""
 
 # Function
 # Open the database and establish connection with it
@@ -48,6 +60,14 @@ def print_most_popular_authors():
     for name, count in all_the_authors:
         print(" {} -- {} views".format(name, count))
 
+# Print days did more than 1% of requests lead to errors
+def print_errors_days():
+    print('\n' + "Days did more than 1% of requests lead to errors:" + '\n')
+    errors_days = get_news(error_requests)
+    for date, percent in errors_days:
+        print("{0:%B %d, %Y} -- {1:.2f} % errors".format(date, percent))
+
 if __name__ == '__main__':
     print_most_popular_articles()
     print_most_popular_authors()
+    print_errors_days()
