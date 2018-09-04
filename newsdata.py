f094470800e12498ac3e4264f74ef4cc95ae73d4
Zhen Bian <zbian002@ucr.edu>
2018-09-03 17:17:22 -0700

    Update the newsdata.py file make The code conforms to the PEP8 style recommendations

--- newsdata.py
+++ newsdata.py
@@ -23,18 +23,21 @@ most_popular_authors = """select authors.name, count(*) as num
 
 # Database query 3: On which days did more than 1% of requests lead to errors?
 error_requests = """select total.date,
-                    round(errors.error_requests * 1.0 / total.requests * 100, 2) as percent
+                    round(errors.error_requests*1.0 / total.requests * 100, 2)
+                    as percent
                     from (
                         select date(time), count(*) as error_requests
                         from log where status like '404%'
                         group by date(time)
                     ) as errors, (
-		                  select date(time), count(*) as requests
-                          from log group by date(log.time)
+                        select date(time), count(*) as requests
+                        from log group by date(log.time)
                     ) as total
                     where total.date = errors.date
-                    and round(errors.error_requests * 1.0 / total.requests * 100, 2) > 1
+                    and round(errors.error_requests*1.0
+                    / total.requests * 100, 2) > 1
                     order by percent desc;"""
+
 
 # Function
 # Open the database and establish connection with it
@@ -46,12 +49,14 @@ def get_news(query):
     db.close()
     return news
 
+
 # Print the most popular three articles of all time
 def print_most_popular_articles():
     print('\n' + "The most popular three articles of all time:" + '\n')
     top_three_articiles = get_news(most_popular_articles)
     for title, count in top_three_articiles:
         print(" \"{}\" -- {} views".format(title, count))
+
 
 # Print the most popular article authors of all time
 def print_most_popular_authors():
@@ -60,12 +65,14 @@ def print_most_popular_authors():
     for name, count in all_the_authors:
         print(" {} -- {} views".format(name, count))
 
+
 # Print days did more than 1% of requests lead to errors
 def print_errors_days():
     print('\n' + "Days did more than 1% of requests lead to errors:" + '\n')
     errors_days = get_news(error_requests)
     for date, percent in errors_days:
         print("{0:%B %d, %Y} -- {1:.2f} % errors".format(date, percent))
+
 
 if __name__ == '__main__':
     print_most_popular_articles()
